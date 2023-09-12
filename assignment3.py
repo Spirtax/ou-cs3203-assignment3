bdca24e30c0ac7e31d43961435dd008160066083
Spirtax <55373074+Spirtax@users.noreply.github.com>
2023-09-12 16:54:20 -0500

    Part 2

--- assignment3.py
+++ assignment3.py
@@ -5,4 +5,12 @@ def sumOfList(input):
         size += i
     return size
 
+def productOfList(input):
+    size = 1
+    for i in input:
+        size *= i
+    return size
+
+
 print(sumOfList([1, 2, 3]))
+print(productOfList([1, 2, 3, 4]))
