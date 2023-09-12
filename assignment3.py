b8d20c6a791308755698759a6a7cd22513c187c1
Spirtax <55373074+Spirtax@users.noreply.github.com>
2023-09-12 16:59:16 -0500

    Part 3

--- assignment3.py
+++ assignment3.py
@@ -1,3 +1,9 @@
+def main():
+    x = input("Input a list of number (Seperate with spaces)")
+    arr = [float(i) for i in x.split()]
+    
+    print("Sum of array: ", sumOfList(arr))
+    print("Product of array: ", productOfList(arr))
 
 def sumOfList(input):
     size = 0
@@ -10,7 +16,3 @@ def productOfList(input):
     for i in input:
         size *= i
     return size
-
-
-print(sumOfList([1, 2, 3]))
-print(productOfList([1, 2, 3, 4]))
